9d16e262b5e51bcd79a4e249518209114c32afa4
Chinmay Bhide <chinmay.bhide154@gmail.com>
2024-03-31 14:35:14 -0700

    sclk now has 50% duty

--- clk_div.sv
+++ clk_div.sv
@@ -11,20 +11,22 @@ module clk_div #(
 
 reg [31:0] counter;
 logic [$clog2(SPI_MAXLEN):0] n_pulses_counter;
+
 always @(posedge clk or negedge rst) begin
     if (!rst) begin
         counter <= 0;
         spi_clk <= 0;
-        n_pulses_counter <= n_pulses;
+        //n_pulses_counter <= n_pulses;
     end 
     
     else begin
-        if(start & n_pulses_counter != 1'b0) begin
+        //if((start != 1'b0) & (n_pulses_counter != 1'b0)) begin
+        if(start != 1'b0 && counter != (SPI_MAXLEN * CLK_DIVIDE) << 2) begin
             spi_clk <= ~spi_clk;
             counter <= counter + 1;
-            n_pulses_counter = n_pulses_counter - 1;
+            //n_pulses_counter = n_pulses_counter - 1;
         end
-
+        
         else begin
             spi_clk <= 1'b0;
             n_pulses_counter <= n_pulses;
@@ -32,5 +34,6 @@ always @(posedge clk or negedge rst) begin
         end
     end
 end
+
 
 endmodule
